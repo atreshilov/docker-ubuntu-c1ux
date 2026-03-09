d55e871fb681e919d9b7767f99efcbcfff8f951d
Alexey Treshilov <atreshilov@gmail.com>
2026-03-10 02:07:17 +0500

    Minor

--- start.sh
+++ start.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 set -e
-#docker compose create
-#docker start c1ux
-#DOCKER_CLI_HINTS=0 docker exec -it c1ux /bin/bash
-#docker stop c1ux
-#docker rm c1ux
-
 docker compose build
 docker compose run --rm --name c1ux ubuntu-c1ux /bin/bash
