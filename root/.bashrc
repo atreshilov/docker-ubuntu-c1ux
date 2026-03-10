c89decdc5c71f60cc8f328c19415dd389dbf1026
Alexey Treshilov <atreshilov@gmail.com>
2026-03-10 15:56:49 +0500

    Call unmount.sh on exit

--- root/.bashrc
+++ root/.bashrc
@@ -1,3 +1,10 @@
 if [ -f ~/.bash_profile ]; then
     . ~/.bash_profile
 fi
+
+my_exit_script() {
+    /root/bin/unmount.sh
+}
+
+# Set the trap to run the function on shell exit
+trap my_exit_script EXIT
--- root/bin/unmount.sh
+++ root/bin/unmount.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 set -e
+if [ ! -f /mnt/disk/my_sparse.img ]; then
+	echo "ERROR: /mnt/disk/my_sparse.img (docker volume mapped to external ./_temp_/my_sparse.img) is not accessible"
+	exit 1
+fi
 cd /
 umount /mnt/img
 losetup -d /dev/loop0
