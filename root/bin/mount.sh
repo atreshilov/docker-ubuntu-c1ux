0c129e8c44a23d87ad00283e00361ce9c743845d
Alexey Treshilov <atreshilov@gmail.com>
2026-03-18 18:03:25 +0500

    Minor

--- root/bin/mount.sh
+++ root/bin/mount.sh
@@ -12,10 +12,10 @@ losetup -P /dev/loop0 /mnt/disk/my_sparse.img
 mkdir -p /mnt/img
 mount -o rw /dev/loop0 /mnt/img
 
-if [ ! -d /mnt/img/app ]; then
-	echo "ERROR: /mnt/img/app is not accessible"
-	exit 1
-fi
+#if [ ! -d /mnt/img/app ]; then
+#	echo "ERROR: /mnt/img/app is not accessible"
+#	exit 1
+#fi
 
 cd /mnt/img/app
 rm -R LiteAPP carlife-release onstarservices-release users-release text-release apa-release calendar-release vpmpro-release infotainment appstore-release nav-release androidauto-release amapauto-debug || true
--- root/bin/unmount.sh
+++ root/bin/unmount.sh
@@ -3,10 +3,10 @@ set -e
 
 echo "Unmount /mnt/disk/my_sparse.img from /mnt/img ..."
 
-if [ ! -d /mnt/img/app ]; then
-	echo "ERROR: /mnt/img/app is not accessible or already unmounted"
-	exit 1
-fi
+#if [ ! -d /mnt/img/app ]; then
+#	echo "ERROR: /mnt/img/app is not accessible or already unmounted"
+#	exit 1
+#fi
 
 cd /
 umount /mnt/img
